f47c553da0eaec805432d6c03096f94c1b4738e4
Mohd Kasif Khan <developer786kasif@gmail.com>
2025-08-15 15:03:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 function firstWord(s) {
   // your code here
+	 let result = "";
+    for (let i = 0; i < s.length; i++) {
+        if (s.charAt(i) === " ") {
+            return result; 
+        }
+        result += s.charAt(i); 
+    }
+    return result; 
 }
-
 // Do not change the code below
-
 const s = prompt("Enter String:");
 alert(firstWord(s));
